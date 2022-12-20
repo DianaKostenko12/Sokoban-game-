a53fb426b35e4b756bea781f74945d9bd580445b
DianaKostenko12 <dianakostenko41@gmail.com>
2022-12-20 21:09:44 +0300

    add moving

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
 
 namespace ConsoleApp1
@@ -7,120 +8,102 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //List<(int, int)> box = new List<(int, int)> { (3, 1), (4, 5), (2, 4) };
+            (int, int) hero = (3, 3);
             char[,] playingField =
             {
-                {' ',' ','#','#','#','#',' '},
-                {'#','#','#',' ',' ','#',' '},
-                {'#','.',' ','$',' ','#',' '},
-                {'#','&','.','#',' ','#',' '},
+                {'#','#','#','#','#','#','#'},
+                {'#','#','#',' ',' ','#','#'},
+                {'#','.',' ',' ',' ',' ','#'},
+                {'#',' ','.',' ',' ',' ','#'},
                 {'#','#',' ',' ','.',' ','#'},
-                {'#',' ','.','&',' ','#',' '},
-                {'#','&',' ','#',' ','#',' '},
-                {'#','#',' ','#',' ','#',' '},
+                {'#',' ','.',' ',' ',' ','#'},
+                {'#',' ',' ','#',' ',' ','#'},
+                {'#','#','#','#','#','#','#'},
             };
-            for (int i = 0; i < playingField.GetLength(0); i++)
-            {
-                for (int j = 0; j < playingField.GetLength(1); j++)
-                {
-                    Console.Write(playingField[i,j] + " ");
-                }
-                Console.WriteLine();
-            }
+            PrintField(playingField,hero/*, box*/);
             while (true)
             {
-                movementFigure(playingField);
+                movementFigure(playingField,ref hero/*, ref box*/);
             }
         }
-        static void movementFigure(char[,] playingField)
+        static void movementFigure(char[,] playingField, ref (int, int) index/*, ref List<(int, int)> box*/)
         {
-            (int, int) index = (0, 0);
             var key = Console.ReadKey().Key;
-            Console.Clear();
-            for (int i = 0; i < playingField.GetLength(0); i++)
+
+            (int, int) way = (0, 0);
+            switch (key)
             {
-                for (int j = 0; j < playingField.GetLength(1); j++)
-                {
-                    if (playingField[i, j] == '$')
-                    {
-                        index.Item1 = i;
-                        index.Item2 = j;
-                    }
-                    else { continue; }
-                }
-            }
-            (int, int) way = (0,0);
-                switch (key)
-                {
-                    case ConsoleKey.LeftArrow:
+                case ConsoleKey.LeftArrow:
                     way = (0, -1);
-                        break;
+                    break;
 
-                    case ConsoleKey.RightArrow:
+                case ConsoleKey.RightArrow:
                     way = (0, 1);
-                        break;
+                    break;
 
-                    case ConsoleKey.UpArrow:
+                case ConsoleKey.UpArrow:
                     way = (-1, 0);
-                        break;
+                    break;
 
-                    case ConsoleKey.DownArrow:
+                case ConsoleKey.DownArrow:
                     way = (1, 0);
-                        break;
-                }
+                    break;
+            }
 
-            if(way.Item1 == 0 & way.Item2 == 0)
+            if (way.Item1 == 0 & way.Item2 == 0)
             {
                 return;
             }
 
-            playingField[index.Item1, index.Item2] = ' ';
-            if (playingField[index.Item1 + way.Item1, index.Item2 + way.Item2] == '#')
+            if (playingField[index.Item1 + way.Item1, index.Item2 + way.Item2] == '#'
+                || playingField[index.Item1 + way.Item1, index.Item2 + way.Item2] == '&')
             {
-                playingField[index.Item1, index.Item2] = '$';
+                return;
             }
             else
             {
-                playingField[index.Item1 + way.Item1, index.Item2 + way.Item2] = '$';
+                index = (index.Item1 + way.Item1, index.Item2 + way.Item2);
             }
 
+            Console.Clear();
+            PrintField(playingField, index/*,box*/);
+
+        }
+        static void PrintField(char[,] playingField, (int, int) index/*, List<(int, int)> box*/)
+       {
             for (int i = 0; i < playingField.GetLength(0); i++)
             {
                 for (int j = 0; j < playingField.GetLength(1); j++)
                 {
-                    Console.Write(playingField[i, j] + " ");
+                    if (i == index.Item1 && j == index.Item2)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.BackgroundColor = ConsoleColor.DarkYellow;
+                        Console.Write("$ ");
+                    }
+                    //else if (i == box[i].Item1 && j == box[i].Item2)
+                    //{
+                    //    Console.ForegroundColor = ConsoleColor.Black;
+                    //    Console.BackgroundColor = ConsoleColor.DarkYellow;
+                    //    Console.Write("& ");
+                    //}
+                    else if (playingField[i, j] == '#')
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.Write(playingField[i, j] + " ");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.BackgroundColor = ConsoleColor.DarkYellow;
+                        Console.Write(playingField[i, j] + " ");
+                    }
                 }
                 Console.WriteLine();
             }
-                //return playingField;
-        }
-        //public static (int, int) GetCoordinatesToMove(ConsoleKey key)
-        //{
-        //    switch (key)
-        //    {
-        //        case ConsoleKey.LeftArrow:
-        //            return (1, 0);
-        //        case ConsoleKey.UpArrow:
-        //            return (0, 1);
-        //        case ConsoleKey.RightArrow:
-        //            return (1, 0);
-        //        case ConsoleKey.DownArrow:
-        //            return (0, 1);
-
-
-
-        //        default:
-        //            return (99,99);
-        //    }
-        //    playingField[index.Item1, index.Item2] = ' ';
-        //    if (playingField[index.Item1, index.Item2 + 1] == '#')
-        //    {
-        //        Console.WriteLine("Немає можливості сюди пройти ¯\\_(ツ)_/¯");
-        //        playingField[index.Item1, index.Item2] = '$';
-        //    }
-        //    else
-        //    {
-        //        playingField[index.Item1, index.Item2 + 1] = '$';
-        //    }
-        //}
+            Console.BackgroundColor = ConsoleColor.Black;
+       }
     }
 }

3a59eee840c74c81ff4ab59375d8ea5c7002e5f3
DianaKostenko12 <dianakostenko41@gmail.com>
2022-12-22 17:04:48 +0300

    add box moving

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using System.Reflection;
 
 namespace ConsoleApp1
@@ -8,26 +9,29 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //List<(int, int)> box = new List<(int, int)> { (3, 1), (4, 5), (2, 4) };
-            (int, int) hero = (3, 3);
             char[,] playingField =
-            {
+           {
                 {'#','#','#','#','#','#','#'},
                 {'#','#','#',' ',' ','#','#'},
-                {'#','.',' ',' ',' ',' ','#'},
+                {'#',' ',' ',' ',' ',' ','#'},
                 {'#',' ','.',' ',' ',' ','#'},
                 {'#','#',' ',' ','.',' ','#'},
                 {'#',' ','.',' ',' ',' ','#'},
                 {'#',' ',' ','#',' ',' ','#'},
                 {'#','#','#','#','#','#','#'},
             };
-            PrintField(playingField,hero/*, box*/);
+            List<(int, int)> indexOfDot = ListDot(playingField);
+            (int, int) way = (0, 0);
+            List<(int, int)> box = new List<(int, int)> { (3, 1), (4, 5), (2, 4) };
+            (int, int) hero = (3, 3);
+           
+            PrintField(playingField, hero, box,way,indexOfDot);
             while (true)
             {
-                movementFigure(playingField,ref hero/*, ref box*/);
+                movementFigure(playingField, ref hero, ref box, indexOfDot);
             }
         }
-        static void movementFigure(char[,] playingField, ref (int, int) index/*, ref List<(int, int)> box*/)
+        static void movementFigure(char[,] playingField, ref (int, int) index, ref List<(int, int)> box, List<(int, int)> indexOfDot)
         {
             var key = Console.ReadKey().Key;
 
@@ -56,10 +60,23 @@ namespace ConsoleApp1
                 return;
             }
 
-            if (playingField[index.Item1 + way.Item1, index.Item2 + way.Item2] == '#'
-                || playingField[index.Item1 + way.Item1, index.Item2 + way.Item2] == '&')
+            if (playingField[index.Item1 + way.Item1, index.Item2 + way.Item2] == '#')
             {
                 return;
+            }
+            else if(BoxList(box, index.Item1 + way.Item1, index.Item2 + way.Item2))
+            {
+                if(playingField[index.Item1 + way.Item1+ way.Item1, index.Item2 + way.Item2+ way.Item2] == '#')
+                {
+                    return;
+                }
+                else
+                {
+                    index = (index.Item1 + way.Item1, index.Item2 + way.Item2);
+                    var boxindex = IndexofTuple(box, index);
+                    box[boxindex] = (box[boxindex].Item1 + way.Item1,
+                        box[boxindex].Item2 + way.Item2);
+                }
             }
             else
             {
@@ -67,11 +84,11 @@ namespace ConsoleApp1
             }
 
             Console.Clear();
-            PrintField(playingField, index/*,box*/);
+            PrintField(playingField, index, box,way,indexOfDot);
 
         }
-        static void PrintField(char[,] playingField, (int, int) index/*, List<(int, int)> box*/)
-       {
+        static void PrintField(char[,] playingField, (int, int) index, List<(int, int)> boxs, (int, int) way, List<(int, int)> indexOfDot)
+        {
             for (int i = 0; i < playingField.GetLength(0); i++)
             {
                 for (int j = 0; j < playingField.GetLength(1); j++)
@@ -82,12 +99,20 @@ namespace ConsoleApp1
                         Console.BackgroundColor = ConsoleColor.DarkYellow;
                         Console.Write("$ ");
                     }
-                    //else if (i == box[i].Item1 && j == box[i].Item2)
-                    //{
-                    //    Console.ForegroundColor = ConsoleColor.Black;
-                    //    Console.BackgroundColor = ConsoleColor.DarkYellow;
-                    //    Console.Write("& ");
-                    //}
+                    else if (BoxList(boxs, i, j))
+                    {
+                        if(ColourOfBox((i, j), indexOfDot))
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Black;
+                        }
+
+                        Console.BackgroundColor = ConsoleColor.DarkYellow;
+                        Console.Write("& ");
+                    }
                     else if (playingField[i, j] == '#')
                     {
                         Console.ForegroundColor = ConsoleColor.White;
@@ -104,6 +129,56 @@ namespace ConsoleApp1
                 Console.WriteLine();
             }
             Console.BackgroundColor = ConsoleColor.Black;
-       }
+        }
+        static bool BoxList(List<(int, int)> box, int i, int j)
+        {
+            for (int k = 0; k < box.Count; k++)
+            {
+                if (box[k].Item1 == i && box[k].Item2 == j)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        static int IndexofTuple(List<(int, int)> box, (int, int) index)
+        {
+            for (int k = 0; k < box.Count; k++)
+            {
+                if (index.Item1 == box[k].Item1 && index.Item2 == box[k].Item2)
+                {
+                    return k;
+                }
+            }
+            return 0;
+        }
+        static bool ColourOfBox((int, int) box, List<(int, int)> indexOfDot)
+        {
+            for (int i = 0; i < indexOfDot.Count; i++)
+            {
+                if (box.Item1 == indexOfDot[i].Item1 && box.Item2 == indexOfDot[i].Item2)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        static List<(int, int)> ListDot(char[,] playingField)
+        {
+            var list = new List<(int, int)>();
+            for (int i = 0; i < playingField.GetLength(0); i++)
+            {
+                for (int j = 0; j < playingField.GetLength(1); j++)
+                {
+                    if (playingField[i, j] == '.')
+                    {
+                        list.Add((i, j));
+                    }
+                }
+            }
+
+            return list;
+        }
     }
 }
